0549a127713a96c6762b8da9ffcff61b920d8c72
awskumarsm2405 <awskumarsm2405@gmail.com>
2022-08-07 18:15:01 +0530

    dev test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,14 +1,14 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-/*it('returns a 201 on successful signup', async () => {
+it('returns a 201 on successful signup', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
       email: 'test@test.com',
       password: 'password'
     })
-    .expect(201);
+    .expect(500);
 });
 
 it('returns a 400 with an invalid email', async () => {
@@ -75,4 +75,4 @@ it('sets a cookie after successful signup', async () => {
     .expect(201);
 
   expect(response.get('Set-Cookie')).toBeDefined();
-});*/
+});
