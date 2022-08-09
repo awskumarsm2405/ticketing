07bc9456375f89ce110c17b6c9112d6c0b5268ae
awskumarsm2405 <awskumarsm2405@gmail.com>
2022-08-10 00:28:44 +0530

    update acctions github

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("start lisint dddddddddddddddd");
+  console.log("start lisint dddddddddddddddd dddddddddd");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
