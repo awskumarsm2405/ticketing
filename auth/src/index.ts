560a8dbf8ab354d0b296a4941897cb1f2a2af461
awskumarsm2405 <awskumarsm2405@gmail.com>
2022-08-29 10:44:03 +0530

    test 3

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("start lisint dddddddddddddddd testtsttstststt");
+  console.log("start lisint  testtsttstststt");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
